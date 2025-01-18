030d21eea2a88d1fa59409b97efba3bc9837a17b
Bool-Pug <jswwolf120@gmail.com>
2025-01-17 19:48:49 -0600

    initial commit

---
+++ src/main/java/frc/robot/RobotContainer.java
@@ -0,0 +1,227 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot;
+
+import com.pathplanner.lib.auto.NamedCommands;
+import edu.wpi.first.math.MathUtil;
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.Filesystem;
+import edu.wpi.first.wpilibj.RobotBase;
+import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.PrintCommand;
+import edu.wpi.first.wpilibj2.command.RunCommand;
+import edu.wpi.first.wpilibj2.command.StartEndCommand;
+import edu.wpi.first.wpilibj2.command.button.CommandJoystick;
+import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+import edu.wpi.first.wpilibj2.command.button.Trigger;
+import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine.Direction;
+import frc.robot.Constants.OperatorConstants;
+import frc.robot.commands.swervedrive.drivebase.AbsoluteDriveAdv;
+import frc.robot.subsystems.CoolArm;
+import frc.robot.subsystems.swervedrive.SwerveSubsystem;
+import frc.robot.subsystems.swervedrive.Vision;
+
+import java.io.File;
+import java.util.function.DoubleSupplier;
+
+import swervelib.SwerveInputStream;
+
+/**
+ * This class is where the bulk of the robot should be declared. Since Command-based is a "declarative" paradigm, very
+ * little robot logic should actually be handled in the {@link Robot} periodic methods (other than the scheduler calls).
+ * Instead, the structure of the robot (including subsystems, commands, and trigger mappings) should be declared here.
+ */
+public class RobotContainer
+{
+
+  // Replace with CommandPS4Controller or CommandJoystick if needed
+  final         CommandJoystick driverJoystick = new CommandJoystick(0);
+
+  final         CommandJoystick copilotController = new CommandJoystick(1);
+
+  // The robot's subsystems and commands are defined here...
+  private final SwerveSubsystem       drivebase  = new SwerveSubsystem(new File(Filesystem.getDeployDirectory(),
+                                                                                "swerve/Omega"));
+  private final Vision vision = drivebase.vision;
+
+  private final CoolArm coolArm = new CoolArm();
+  // Applies deadbands and inverts controls because joysticks
+  // are back-right positive while robot
+  // controls are front-left positive
+  // left stick controls translation
+  // right stick controls the rotational velocity 
+  // buttons are quick rotation positions to different ways to face
+  // WARNING: default buttons are on the same buttons as the ones defined in configureBindings
+
+  // AbsoluteDriveAdv closedAbsoluteDriveAdv = new AbsoluteDriveAdv(drivebase,
+  //                                                                () -> -MathUtil.applyDeadband(driverXbox.getLeftY(),
+  //                                                                                              OperatorConstants.LEFT_Y_DEADBAND),
+  //                                                                () -> -MathUtil.applyDeadband(driverXbox.getLeftX(),
+  //                                                                                              OperatorConstants.DEADBAND),
+  //                                                                () -> -MathUtil.applyDeadband(driverXbox.getRightX(),
+  //                                                                                              OperatorConstants.RIGHT_X_DEADBAND),
+  //                                                                driverXbox.getHID()::getYButtonPressed,
+  //                                                                driverXbox.getHID()::getAButtonPressed,
+  //                                                                driverXbox.getHID()::getXButtonPressed,
+  //                                                                driverXbox.getHID()::getBButtonPressed);
+
+  /**
+   * Converts driver input into a field-relative ChassisSpeeds that is controlled by angular velocity.
+   */
+  SwerveInputStream driveAngularVelocity = SwerveInputStream.of(drivebase.getSwerveDrive(),
+                                                                () -> driverJoystick.getY() * -1,
+                                                                () -> driverJoystick.getX() * -1)
+                                                            .withControllerRotationAxis(() -> driverJoystick.getTwist() * -0.5)
+                                                            .deadband(OperatorConstants.DEADBAND)
+                                                            .scaleTranslation(0.8)
+                                                            .allianceRelativeControl(true);
+
+  /**
+   * Clone's the angular velocity input stream and converts it to a fieldRelative input stream.
+   */
+  // SwerveInputStream driveDirectAngle = driveAngularVelocity.copy().withControllerHeadingAxis(driverXbox::getRightX,
+  //                                                                                            driverXbox::getRightY)
+  //                                                          .headingWhile(true);
+
+
+  // Applies deadbands and inverts controls because joysticks
+  // are back-right positive while robot
+  // controls are front-left positive
+  // left stick controls translation
+  // right stick controls the desired angle NOT angular rotation
+  //Command driveFieldOrientedDirectAngle = drivebase.driveFieldOriented(driveAngularVelocity);
+
+  // Applies deadbands and inverts controls because joysticks
+  // are back-right positive while robot
+  // controls are front-left positive
+  // left stick controls translation
+  // right stick controls the angular velocity of the robot
+  Command driveFieldOrientedAnglularVelocity = drivebase.driveFieldOriented(driveAngularVelocity);
+
+  Command driveSetpointGen = drivebase.driveWithSetpointGeneratorFieldRelative(driveAngularVelocity);
+
+  SwerveInputStream driveAngularVelocitySim = SwerveInputStream.of(drivebase.getSwerveDrive(),
+                                                                   () -> -driverJoystick.getY(),
+                                                                   () -> -driverJoystick.getX())
+                                                               .withControllerRotationAxis(() -> driverJoystick.getRawAxis(2))
+                                                               .deadband(OperatorConstants.DEADBAND)
+                                                               .scaleTranslation(0.8)
+                                                               .allianceRelativeControl(true);
+  // Derive the heading axis with math!
+  SwerveInputStream driveDirectAngleSim     = driveAngularVelocitySim.copy()
+                                                                     .withControllerHeadingAxis(() -> Math.sin(
+                                                                                                    driverJoystick.getRawAxis(
+                                                                                                        2) * Math.PI) * (Math.PI * 2),
+                                                                                                () -> Math.cos(
+                                                                                                    driverJoystick.getRawAxis(
+                                                                                                        2) * Math.PI) *
+                                                                                                      (Math.PI * 2))
+                                                                     .headingWhile(true);
+
+  Command driveFieldOrientedDirectAngleSim = drivebase.driveFieldOriented(driveDirectAngleSim);
+
+  Command driveSetpointGenSim = drivebase.driveWithSetpointGeneratorFieldRelative(driveDirectAngleSim);
+
+  /**
+   * The container for the robot. Contains subsystems, OI devices, and commands.
+   */
+  public RobotContainer()
+  {
+    // Configure the trigger bindings
+    configureBindings();
+    DriverStation.silenceJoystickConnectionWarning(true);
+    NamedCommands.registerCommand("test", Commands.print("I EXIST"));
+
+    // Shuffleboard.getTab("testing").addDouble("Distance to Tag 16", vision::getLatestTag16Distance);
+    driverJoystick.button(14).onTrue(new InstantCommand( () -> this.getTag16Distance()  )) ;
+  }
+
+  public void getTag16Distance(){
+    if(vision != null){
+      System.out.println(vision.getLatestTag16Distance());
+      
+    }
+    
+  }
+
+  /**
+   * Use this method to define your trigger->command mappings. Triggers can be created via the
+   * {@link Trigger#Trigger(java.util.function.BooleanSupplier)} constructor with an arbitrary predicate, or via the
+   * named factories in {@link edu.wpi.first.wpilibj2.command.button.CommandGenericHID}'s subclasses for
+   * {@link CommandXboxController Xbox}/{@link edu.wpi.first.wpilibj2.command.button.CommandPS4Controller PS4}
+   * controllers or {@link edu.wpi.first.wpilibj2.command.button.CommandJoystick Flight joysticks}.
+   */
+  private void configureBindings()
+  {
+    // (Condition) ? Return-On-True : Return-on-False
+    drivebase.setDefaultCommand(driveFieldOrientedAnglularVelocity);
+
+    driverJoystick.button(13).onTrue(Commands.runOnce(drivebase::zeroGyro));
+
+    Command autoAim = drivebase.aimAtSpeaker(5);
+    autoAim.addRequirements(drivebase);
+    driverJoystick.button(3).whileTrue(autoAim);
+
+
+    // driverJoystick.button(6).whileTrue(new StartEndCommand(() -> coolArm.SetAngleMotor(0.1), () -> coolArm.SetAngleMotor(0), coolArm));
+    // driverJoystick.button(9).whileTrue(new StartEndCommand(() -> coolArm.SetAngleMotor(-0.1), () -> coolArm.SetAngleMotor(0), coolArm));
+
+    // driverJoystick.button(5).whileTrue(new StartEndCommand(() -> coolArm.SetElevatorMotor(0.1), () -> coolArm.SetElevatorMotor(0), coolArm));
+    // driverJoystick.button(8).whileTrue(new StartEndCommand(() -> coolArm.SetElevatorMotor(-0.1), () -> coolArm.SetElevatorMotor(0), coolArm));
+
+    // driverJoystick.button(15).whileTrue(coolArm.sysIdRoutine.dynamic(Direction.kForward));
+    // driverJoystick.button(12).whileTrue(coolArm.sysIdRoutine.quasistatic(Direction.kForward));
+
+    // driverJoystick.button(16).whileTrue(coolArm.sysIdRoutine.dynamic(Direction.kReverse));
+    // driverJoystick.button(11).whileTrue(coolArm.sysIdRoutine.quasistatic(Direction.kReverse));
+
+    coolArm.setDefaultCommand(new RunCommand(() -> coolArm.SetAngleSetpoint(driverJoystick.getThrottle() *-90 + 90),coolArm));
+
+    copilotController.button(1).onTrue(new InstantCommand(()->coolArm.SetArmAction(CoolArm.ArmAction.Place)));
+    copilotController.button(6).onTrue(new InstantCommand(()->coolArm.SetArmAction(CoolArm.ArmAction.L1)));
+    copilotController.button(7).onTrue(new InstantCommand(()->coolArm.SetArmAction(CoolArm.ArmAction.L2)));
+    copilotController.button(11).onTrue(new InstantCommand(()->coolArm.SetArmAction(CoolArm.ArmAction.L3)));
+    copilotController.button(10).onTrue(new InstantCommand(()->coolArm.SetArmAction(CoolArm.ArmAction.L4)));
+
+    copilotController.button(8).onTrue(new InstantCommand(()->coolArm.SetArmAction(CoolArm.ArmAction.Travel)));
+    copilotController.button(9).onTrue(new InstantCommand(()->coolArm.SetArmAction(CoolArm.ArmAction.Pickup)));
+
+    copilotController.button(3).whileTrue(new StartEndCommand(()->coolArm.SetElevatorMotor(1), ()->coolArm.SetElevatorMotor(0)));
+    copilotController.button(2).whileTrue(new StartEndCommand(()->coolArm.SetElevatorMotor(-1),()->coolArm.SetElevatorMotor(0)));
+
+
+  }
+
+  /**
+   * Use this to pass the autonomous command to the main {@link Robot} class.
+   *
+   * @return the command to run in autonomous
+   */
+  public Command getAutonomousCommand()
+  {
+    // An example command will be run in autonomous
+    return drivebase.getAutonomousCommand("New Auto");
+  }
+
+  public void setDriveMode()
+  {
+    configureBindings();
+  }
+
+  public void setMotorBrake(boolean brake)
+  {
+    drivebase.setMotorBrake(brake);
+  }
+
+  public void disabledPeriodic(){
+    coolArm.SetAngleSetpoint(coolArm.absAngleEncoder.getPosition());
+  }
+}
---
+++ src/main/java/frc/robot/subsystems/CoolArm.java
@@ -0,0 +1,166 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import static edu.wpi.first.units.Units.Degrees;
+import static edu.wpi.first.units.Units.DegreesPerSecond;
+import static edu.wpi.first.units.Units.Seconds;
+import static edu.wpi.first.units.Units.Volts;
+
+import java.util.function.Predicate;
+
+import javax.annotation.processing.SupportedOptions;
+
+import org.dyn4j.geometry.Rotation;
+
+import com.revrobotics.RelativeEncoder;
+import com.revrobotics.spark.SparkAbsoluteEncoder;
+import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.SparkRelativeEncoder;
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+
+import edu.wpi.first.math.controller.ArmFeedforward;
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.math.proto.Wpimath;
+import edu.wpi.first.units.AngleUnit;
+import edu.wpi.first.units.TimeUnit;
+import edu.wpi.first.units.Units;
+import edu.wpi.first.units.VoltageUnit;
+import edu.wpi.first.units.measure.Angle;
+import edu.wpi.first.units.measure.Voltage;
+import edu.wpi.first.units.Units.*;
+import edu.wpi.first.wpilibj.motorcontrol.Spark;
+import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj.sysid.SysIdRoutineLog;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine;
+import frc.robot.Robot;
+import frc.robot.RobotContainer;
+import frc.robot.Constants.CoolArmConstants;
+import edu.wpi.first.wpilibj.sysid.SysIdRoutineLog.State;;
+
+public class CoolArm extends SubsystemBase {
+
+  public static enum ArmAction{
+    L1,
+    L2,
+    L3,
+    L4,
+    Travel,
+    Pickup,
+    Place
+  }
+
+  private SparkMax angleMotor = new SparkMax(CoolArmConstants.angleCANID, MotorType.kBrushless);
+  public SparkAbsoluteEncoder absAngleEncoder = angleMotor.getAbsoluteEncoder();
+  private ArmFeedforward armFFController = new ArmFeedforward(CoolArmConstants.kS, CoolArmConstants.kG, CoolArmConstants.kV);
+  private PIDController armPIDController = new PIDController(CoolArmConstants.kP, CoolArmConstants.kI, CoolArmConstants.kD);
+  private double angleSetpoint = 5;
+  private double elevatorSetpoint = 0;
+  private double elevatorTolerance = 5;
+
+
+  public SparkMax elevatorMotor = new SparkMax(CoolArmConstants.elevatorCANID, MotorType.kBrushless);
+  public RelativeEncoder elevatorEncoder = elevatorMotor.getEncoder();
+
+  // public SysIdRoutine sysIdRoutine = new SysIdRoutine(
+  //   new SysIdRoutine.Config(Volts.of( 0.15 ).per(Units.Seconds), Volts.of(0.7), Seconds.of(10)),
+  //   //new SysIdRoutine.Config(),
+  //   new SysIdRoutine.Mechanism(this::voltageDrive, this::logMotors, this));
+
+  /** Creates a new CoolArm. */
+  public CoolArm() {
+    // Creates a SysIdRoutine
+    
+      
+    Shuffleboard.getTab("Arm Sysid Testing").addDouble("Absolute Angle", absAngleEncoder::getPosition);
+    Shuffleboard.getTab("Arm Sysid Testing").addDouble("Angle Setpoint", () -> angleSetpoint);
+    Shuffleboard.getTab("Arm Sysid Testing").addDouble("Angle Motor Current", angleMotor::getOutputCurrent);
+    Shuffleboard.getTab("Arm Sysid Testing").addDouble("Elevator Position", elevatorEncoder::getPosition);
+    armPIDController.setIZone(20);
+
+    angleSetpoint = absAngleEncoder.getPosition();
+    SetElevatorEncoder(0);
+    elevatorSetpoint = elevatorEncoder.getPosition();
+
+    //Shuffleboard.getTab("Arm Sysid Testing").add(armPIDController);
+    SmartDashboard.putData(armPIDController);
+  }
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+    double absAngle = absAngleEncoder.getPosition();
+    SetAngleMotor(armPIDController.calculate(absAngle,angleSetpoint ) + armFFController.calculate( ( (angleSetpoint - 90) / 180) * Math.PI, 0));
+
+    // if(elevatorEncoder.getPosition() > elevatorSetpoint + elevatorTolerance){
+    //   SetElevatorMotor(-1);
+    // }
+    // else if(elevatorEncoder.getPosition() < elevatorSetpoint - elevatorTolerance){
+    //   SetElevatorMotor(1);
+    // }
+    // else{
+    //   SetElevatorMotor(0);
+    // }
+  }
+
+  
+
+  public void SetArmAction(ArmAction newAction){
+    double newAngleSP = absAngleEncoder.getPosition();
+
+    switch(newAction){
+      case L1:
+        newAngleSP = CoolArmConstants.kL1PrepAngleSP;
+        break;
+      case L2:
+        newAngleSP = CoolArmConstants.kL2PrepAngleSP;
+        break;
+      case L3:
+        newAngleSP = CoolArmConstants.kL3PrepAngleSP;
+        break;
+      case L4:
+        newAngleSP = CoolArmConstants.kL4PrepAngleSP;
+        break;
+      case Travel:
+        newAngleSP = CoolArmConstants.kTravelAngleSP;
+        break;
+      case Pickup:
+        newAngleSP = CoolArmConstants.kPickupAngleSP;
+        break;
+      case Place:
+        newAngleSP += CoolArmConstants.kPlaceSPChange;
+        break;
+    }
+
+    SetAngleSetpoint(newAngleSP);
+      
+  }
+
+  public void SetAngleSetpoint(double sp){
+    angleSetpoint = sp;
+  }
+
+  public void SetAngleMotor(double speed){
+    //angleMotor.setVoltage(speed);
+  }
+
+  public void SetElevatorMotor(double voltage){
+    elevatorMotor.setVoltage(voltage);
+  }
+
+  public void SetElevatorEncoder(double newValue){
+    elevatorEncoder.setPosition(newValue);
+  }
+
+//   public void voltageDrive(Voltage volts){
+//       angleMotor.setVoltage(volts);
+//   }
+
+//   public void logMotors(SysIdRoutineLog log){
+//     log.motor("AngleMotor").voltage(Volts.of( angleMotor.getAppliedOutput()*angleMotor.getBusVoltage() ) ).angularPosition(Degrees.of(absAngleEncoder.getPosition())).angularVelocity(DegreesPerSecond.of(absAngleEncoder.getVelocity()));
+// }
+}
